c33a96ce044bcff6e89bedaf88f5d8bbff8c3890
Yujeong Seo <144188882+yujeongaz@users.noreply.github.com>
2023-09-21 23:21:50 +0900

    [Bronze V] Title: 구구단, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#define _CRT_SECURE_NO_WARNINGS
+
+int main() {
+  int n;
+
+  scanf("%d", &n);
+  for(int i = 1; i <= 9; i++) {
+    printf("%d * %d = %d\n", n, i, n * i);
+  }
+  
+  return 0;
+}
